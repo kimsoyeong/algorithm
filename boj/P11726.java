798ac9310cbda17c41fabf10f30e3239dfabdf24
Soyeong Kim <43427380+kimsoyeong@users.noreply.github.com>
2022-02-28 15:19:01 +0900

    Update P11726.java

--- boj/P11726.java
+++ boj/P11726.java
@@ -8,16 +8,15 @@ public class P11726 { // 2xn 타일링
 
         int n = Integer.parseInt(br.readLine());
 
-        long[] dp = new long[1001];
-        System.out.println(Long.MAX_VALUE);
+        int[] dp = new int[1001];
         dp[1] = 1;
         dp[2] = 2;
 
         for(int i=3; i<1001; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
         }
 
-        System.out.println(dp[n] % 10007);
+        System.out.println(dp[n]);
 
     }
 }
