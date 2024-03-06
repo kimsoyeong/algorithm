7244cc9d68ada042ddc7a9d16c4bd2dd5ab55fab
Soyeong Kim <soyeong@csap.snu.ac.kr>
2024-03-06 19:09:27 +0900

    1859. 백만 장자 프로젝트 D2

---
+++ Samsung/ss1859.py
@@ -0,0 +1,30 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    price = list(map(int, input().split()))
+
+    pocket = 0
+    money = 0
+
+    max_price = price[-1]
+    for i in range(N-2, -1, -1):
+        if max_price > price[i]:
+            money += max_price - price[i]  # 사고 팔고
+        else:
+            max_price = price[i]  # 새로운 매매가
+
+    # for i in range(N):
+    #     if (i < len(price) - 1) and \
+    #             (max(price[i:]) > price[i]):  생
+    # max함수 사용 시, 7개 test case만 통과하고 Runtime error 발생
+    # 아마도, memory 초과 또는 실행시간 초과
+    #         # buy one
+    #         money -= price[i]
+    #         pocket += 1
+    #     else:
+    #         # sell all in pocket
+    #         money += price[i] * pocket
+    #         pocket = 0
+
+    print(f"#{test_case} {money}")
