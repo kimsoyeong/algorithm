bdb9ec2223e137f1b6a7e3165097b9eb586122bb
Soyeong Kim <soyeong@csap.snu.ac.kr>
2024-03-08 12:12:42 +0900

    1954. 달팽이 숫자 - 2번째 solution
    - dx, dy (di, dj) 사용해서 다시 푼 방법

--- Samsung/ss1954.py
+++ Samsung/ss1954.py
@@ -3,39 +3,33 @@ T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
 
-    x = -1
-    y = 0
+    arr = [[0] * N for _ in range(N)]
+    arr[0][0] = 1  # first element
 
-    curr = 1
-    cords = [[0] * N for _ in range(N)]
-    for _ in range(N):
-        x += 1
-        cords[y][x] = curr
-        # print(f"{curr}: [{y}][{x}]")
-        curr += 1
+    # direction
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
+    dr = 0
 
-    direction = 0
-    cnt = N - 1
-    while cnt > 0:
-        for _ in range(2):
-            for _ in range(cnt):
-                if direction % 4 == 0:
-                    y += 1
-                elif direction % 4 == 1:
-                    x -= 1
-                elif direction % 4 == 2:
-                    y -= 1
-                else:  # 3
-                    x += 1
+    i = 0
+    j = 0
+    cnt = 2
+    while cnt <= N * N:
+        # next indices
+        ni = i + di[dr]
+        nj = j + dj[dr]
 
-                cords[y][x] = curr
-                # print(f"{curr} - {direction % 4}: [{y}][{x}]")
-                curr += 1
-            direction += 1
-        cnt -= 1
+        if (0 <= ni < N) and (0 <= nj < N) \
+                and (arr[ni][nj] == 0):
+            i, j = ni, nj
+            arr[i][j] = cnt
+            cnt += 1
+        else:  # change direction
+            dr = (dr + 1) % 4
 
     print(f"#{test_case}")
-    for cord in cords:
-        for c in cord:
+
+    for a in arr:
+        for c in a:
             print(c, end=" ")
         print()
