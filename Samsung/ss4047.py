a1a1aafebfe6f13a65ed057ca1a4c44b5f012d16
kimsoyeong <soyeong.kim9@gmail.com>
2024-03-11 16:46:58 +0900

    4047. 영준이의 카드 카운팅 D3

---
+++ Samsung/ss4047.py
@@ -0,0 +1,31 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    ln = input().strip()
+
+    cards = {
+        'S': [],
+        'D': [],
+        'H': [],
+        'C': []
+    }
+
+    error = False
+
+    for i in range(0, len(ln), 3):
+        card_shape = ln[i]
+        card_number = ln[i + 1: i + 3]
+
+        if card_number in cards[card_shape]:
+            error = True
+            break
+        else:
+            cards[card_shape].append(card_number)
+
+    if error:
+        print(f"#{test_case} ERROR")
+    else:
+        print(f"#{test_case}", end=" ")
+        for s in ['S', 'D', 'H', 'C']:
+            print(13 - len(cards[s]), end=" ")
+        print()
