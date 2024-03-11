72ddd2a7c1ca36c6773e9eb357f38e7cdfc617fa
kimsoyeong <soyeong.kim9@gmail.com>
2024-03-11 16:48:12 +0900

    4047. 영준이의 카드 카운팅 D3 - 2번째 solution
    - for문에도 else를 적용할 수 있었다! (문어박사님)

--- Samsung/ss4047.py
+++ Samsung/ss4047.py
@@ -10,20 +10,15 @@ for test_case in range(1, T + 1):
         'C': []
     }
 
-    error = False
-
     for i in range(0, len(ln), 3):
         card_shape = ln[i]
         card_number = ln[i + 1: i + 3]
 
         if card_number in cards[card_shape]:
-            error = True
+            print(f"#{test_case} ERROR")
             break
         else:
             cards[card_shape].append(card_number)
-
-    if error:
-        print(f"#{test_case} ERROR")
     else:
         print(f"#{test_case}", end=" ")
         for s in ['S', 'D', 'H', 'C']:
