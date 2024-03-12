cef54f8f0f5ba13ae52f3d15a16ecb846f78c9b3
kimsoyeong <soyeong.kim9@gmail.com>
2024-03-13 01:33:21 +0900

    2567. 색종이 - 2 S4 (열방향 순회 solution)

--- boj/boj2567.py
+++ boj/boj2567.py
@@ -7,18 +7,34 @@ for _ in range(N):
 
     for i in range(B, B + 10):
         for j in range(A, A + 10):
-            arr[i+1][j+1] = 1
-
-# 상, 하, 좌, 우
-dys = [1, -1, 0, 0]
-dxs = [0, 0, -1, 1]
+            arr[i + 1][j + 1] = 1
 
 dulae = 0
+
+# 행 방향 순회
 for i in range(1, 101):
-    for j in range(1, 101):
-        if arr[i][j] == 1:
-            for dy, dx in zip(dys, dxs):
-                if arr[i + dy][j + dx] == 0:
-                    dulae += 1
+    for j in range(1, 102):
+        # (j) 왼쪽 값(j - 1)과 비교하므로, 100번째 입력의 오른쪽 가장자리를 확인하려면 101번 index까지 확인해야 한다.
+        if arr[i][j] != arr[i][j - 1]:
+            dulae += 1
+
+# 열 방향 순회
+for j in range(1, 101):
+    for i in range(1, 102):
+        # (i) 위쪽 값(i - 1)과 비교하므로, 100번째 입력의 위쪽 가장자리를 확인하려면 101번 index까지 확인해야 한다.
+        if arr[i][j] != arr[i - 1][j]:
+            dulae += 1
+
+# 1st solution: 전체 순회
+# # 상, 하, 좌, 우
+# dys = [1, -1, 0, 0]
+# dxs = [0, 0, -1, 1]
+#
+# for i in range(1, 101):
+#     for j in range(1, 101):
+#         if arr[i][j] == 1:
+#             for dy, dx in zip(dys, dxs):
+#                 if arr[i + dy][j + dx] == 0:
+#                     dulae += 1
 
 print(dulae)
