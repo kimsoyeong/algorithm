c953156e3b5048243e833d3078517e8ae6942391
kimsoyeong <soyeong.kim9@gmail.com>
2024-03-26 15:50:17 +0900

    2839. 설탕배달 S4 - 2번째 solution
    - 문어박사님 문제 설명 참고
    - while => for: 더 가독성 좋은 코드
    - BUT 속도는 더 느려짐

--- boj/boj2839.py
+++ boj/boj2839.py
@@ -3,15 +3,12 @@ N = int(input())
 cnt5 = 0
 cnt3 = 0
 
-cnt5 = N // 5  # 봉지 수가 최소인 경우는 전부 5kg 봉지로 옮기는 경우
-
-while cnt5 > -1:
+# cnt5: 5kg 봉지 수; 봉지 수가 최소인 경우는 전부 5kg 봉지로 옮기는 경우
+for cnt5 in range(N//5, -1 ,-1):  # 딱 떨어지지 않으면,  # 5kg 봉지 수를 하나 줄인다.
     cnt3 = (N - cnt5 * 5) // 3  # 남은 설탕 kg에서 가능한 3kg 봉지 개수 계산
 
     if cnt5 * 5 + cnt3 * 3 == N:  # 딱 떨어지는 지 확인
         print(cnt5 + cnt3)
         break
-    else:  # 딱 떨어지지 않으면
-        cnt5 -= 1  # 5kg 봉지 수를 하나 줄인다.
 else:
     print(-1)
