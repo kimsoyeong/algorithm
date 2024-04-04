241581449268dba77e96584c91d031a7039e4bca
kimsoyeong <soyeong.kim9@gmail.com>
2024-04-04 19:26:37 +0900

    5208. 전기버스2 D3
    - dfs 초기 호출 시, 배터리 잔량 "-1" 놓칠 뻔.
    - 가지치기 없으면, 시간초과 발생.
    - 가지치기 하는 경우: 유망한 답이 먼저 나오는 방향으로 dfs() 호출

--- SWEA/ss5208.py
+++ SWEA/ss5208.py
@@ -9,9 +9,11 @@ def dfs(n, cnt, battery):
         ans = min(ans, cnt)
         return
 
-    dfs(n + 1, cnt + 1, M[n] - 1)
+    # 가지치기를 고려하는 경우: 좋은(유망한) 답이 먼저 나오는 방향으로 호출
+    # 최소 값을 찾아야하므로, 최대한 교체하지 않는 것이 유망한 답!
     if battery > 0:
-        dfs(n + 1, cnt, battery - 1)
+        dfs(n + 1, cnt, battery - 1)  # 교체 X
+    dfs(n + 1, cnt + 1, M[n] - 1)  # 교체 O
 
 
 T = int(input())
