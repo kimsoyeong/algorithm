21590f81d1175d12a70760b39b8e9a08ffac3f5f
kimsoyeong <soyeong.kim9@gmail.com>
2024-04-04 18:30:34 +0900

    1952. 수영장 - 모의역량 - 2번째 solution
    - 문어박사님 풀이: Greedy 방법 -> 그 정도의 복잡도.

---
+++ SWEA/ss1952-2.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    day, mon, mon3, year = map(int, input().split())
+    lst = [0] + list(map(int, input().split()))
+
+    arr = [0] * 13
+    for i in range(1, 13):
+        mn = arr[i-1] + lst[i] * day  # 이전 달까지 비용 + 일일권 비용
+        mn = min(mn, arr[i - 1] + mon)  # 월간권 비용 (비교)
+        if i >= 3:
+            mn = min(mn, arr[i - 3] + mon3)  # 분기권(3달) 비용 (비교)
+        if i >= 12:
+            mn = min(mn, arr[i - 12] + year) # 연간권 비용 (비교)
+
+        arr[i] = mn  # i번째 달의 최소 비용
+
+    print(f"#{test_case} {mn}")
