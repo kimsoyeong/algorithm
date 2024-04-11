6666c2ef8f21d112d493b9161041eca9b6fe4eea
kimsoyeong <soyeong.kim9@gmail.com>
2024-04-12 03:26:33 +0900

    2105. 디저트 카페 - 모의역량 - 3번째 solution
    - 내 풀이: DFS with 종료조건, 가지치기 w/o visited 배열
        - 가지치기: 사각형 반을 돌았는데, 2배 값(전체 방문 카페 수; 중복X일 때 가정) 이미 정답(ans)보다 작은 경우
        - 935ms (기존: 1958ms < 4904ms < 5410ms)
        - 이전에 "가지치기"라고 생각했던 게 사실은 종료 조건이었음. 이 문제에서는 종료조건과 정답처리가 다르게 처리되어야 함.

--- SWEA/ss2105.py
+++ SWEA/ss2105.py
@@ -2,12 +2,23 @@ directions = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
 
 
 def dfs(i, j, cnt, tour, d):
-    global si, sj, ans
+    """
+    :param i: arr i좌표
+    :param j: arr j좌표
+    :param cnt: 현재 방문 카페의 수 (디저트 중복X)
+    :param tour: 방문한 카페들의 디저트 종류 배열 (중복X)
+    :param d: 투어 이동 방향
+    """
+    global ans
 
-    if d > 3:  # 가지치기
+    # 가지치기: 투어(사각형)의 반을 돌았는데(전체 방문 카페 수는 현재 cnt의 2배가 됨), 이미 정답보다 작은 경우
+    if d == 2 and cnt * 2 <= ans:
         return
 
-    if d == 3 and i == si and j == sj:  # 정답 처리
+    if d > 3:  # 종료조건 (무조건 종료)
+        return
+
+    if d == 3 and i == si and j == sj:  # 정답 처리: 마지막 방향 and 시작점 도착
         ans = max(ans, cnt)
         return
 
@@ -27,11 +38,12 @@ for test_case in range(1, T + 1):
     # visited 배열 사용 X: 어차피 방향 전환하면서, 시작점 외에 중복 방문은 불가능하다.
     ans = 0
 
-    for i in range(N):
+    for i in range(N):  # 실제 필요 범위는 N - 2 전까지 => 시간이 더 오래 걸려서 그냥 N으로 둠
         for j in range(1, N - 1):
             si = i
             sj = j
             dfs(i, j, 0, [arr[i][j]], 0)
+            # dfs(i, j, 0, [], 0)  # tour를 빈 배열로 넘겨주면, for문 안에서 ni == si and nj == sj 조건이 필요없어지나 실제 실행 시간은 더 오래걸렸다.
 
     if ans <= 1: ans = -1
     print(f"#{test_case} {ans}")
